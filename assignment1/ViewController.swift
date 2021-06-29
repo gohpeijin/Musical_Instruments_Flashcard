7df10660540a4733265f51aa1729e89e1c8122e3
gohpeijin <gohpeijin@gohpeijins-Mac.local>
2021-06-29 11:20:32 -0700

    feat - added scroll view so landscape mode can be viewed

--- assignment1/ViewController.swift
+++ assignment1/ViewController.swift
@@ -1,7 +1,9 @@
 //
 //  ViewController.swift
 //  assignment1
-//
+//  This application content of two view
+//  1. Flashcard View
+//  2. Collection View (View all the card together)
 //  Created by gohpeijin on 26/06/2021.
 //  Copyright © 2021 pj. All rights reserved.
 //
@@ -10,20 +12,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var instrumentCollectionView: UICollectionView!
+    // 1. single flashcard view code
+    @IBOutlet weak var singleView: UIView! // the view of the single card view
+    @IBOutlet weak var singleScrollView: UIScrollView! // the scroll view of the sinlge card view
+    @IBOutlet weak var buttonLeft: UIButton! // move to previous image
+    @IBOutlet weak var buttonRight: UIButton! // move to next image
+    @IBOutlet weak var imagePlaceholder: UIImageView!
+    @IBOutlet weak var labelCounter: UILabel! // display the number of the instrument viewing
+    @IBOutlet weak var instrumentName: UILabel!
     
-    @IBOutlet weak var singleView: UIView! // the view of the
+    // 2. collection view code
+    @IBOutlet weak var instrumentCollectionView: UICollectionView!  // the view for all the card at once
+    let imageSize = 180 //image size of the instruments in collection view
     
-    let imageSize = 180
-    
+    // Define the paramters of the instrument
     struct instrument {
         var name : String
         var image : String
     }
-    
     var instrumentlist: [instrument] = []
-    var startindex = 0
-    //create instrument and store in the list
+    var startindex = 0 // strat index for the instrument to be shown
+    
+    // Create instrument and store in the list
     func createArray () -> [instrument] {
         var tempInstrument: [instrument] = []
         
@@ -60,60 +70,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         instrumentlist = createArray()
-
+        
+        // 1. single flashcard view code
+        // set the size of the scrolling view to ensure objects is scrollable in landscape mode
+        singleScrollView.contentSize = singleView.frame.size
+        setImage()
+        
+        // 2. collection view code
         // set the collectionview cell size
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: imageSize, height: imageSize)
         instrumentCollectionView.collectionViewLayout = layout
         
-        // register the cell to the collection
-        instrumentCollectionView.register(InstrumentViewCell.nib(), forCellWithReuseIdentifier: InstrumentViewCell.identifier)
-        
+        instrumentCollectionView.register(InstrumentViewCell.nib(), forCellWithReuseIdentifier: InstrumentViewCell.identifier) // register the cell to the collection
         instrumentCollectionView.delegate = self
         instrumentCollectionView.dataSource = self
-        setImage()
     }
     
-    @IBOutlet weak var buttonLeft: UIButton!
-    @IBOutlet weak var buttonRight: UIButton!
-    
-    @IBAction func buttonLeft(_ sender: UIButton) {
-        startindex -= 1
-        setImage()
-    }
-    @IBAction func buttonRight(_ sender: UIButton) {
-        startindex += 1
-        setImage()
-    }
-    @IBOutlet weak var imagePlaceholder: UIImageView!
-    
-    @IBOutlet weak var labelCounter: UILabel!
+    // 1. single flashcard view code
+    @IBAction func buttonLeft(_ sender: UIButton) { startindex -= 1; setImage()}
+    @IBAction func buttonRight(_ sender: UIButton) {startindex += 1; setImage()}
+   
     func setImage(){
-        instrumentName.alpha = 0
+        instrumentName.alpha = 0 // ensure the label is hidden
         imagePlaceholder.image = (UIImage(named: instrumentlist[startindex].image)!)
         instrumentName.text = instrumentlist[startindex].name
         labelCounter.text = String(startindex+1)
-        if (startindex == 0){
-           buttonLeft.isHidden = true
-        }
-        else{
-            buttonLeft.isHidden = false
-        }
         
-        if (startindex == instrumentlist.count-1){
-            buttonRight.isHidden = true
-        }
-        else{
-            buttonRight.isHidden = false
-        }
+        if (startindex == 0){buttonLeft.isHidden = true}
+        else{buttonLeft.isHidden = false}
+        
+        if (startindex == instrumentlist.count-1){buttonRight.isHidden = true}
+        else{buttonRight.isHidden = false}
+        
     }
 
-    @IBOutlet weak var instrumentName: UILabel!
+    // toggle the label to visible when user interact to the magnifying glass button
     @IBAction func buttonToggle(_ sender: UIButton) {
         if (instrumentName.alpha == 1) {
             UIView.animate(withDuration: 0.2){
                 self.instrumentName.alpha = 0
-                
             }
         }
         else {
@@ -129,19 +125,23 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    // to cahnge the view of 1. flashcard mode to 2. collection mode or vise versa
     @IBAction func buttonChangeView(_ sender: UIButton) {
         if(singleView.isHidden == false){
-            singleView.isHidden = true
+            singleScrollView.isHidden = true
             instrumentCollectionView.isHidden = false
         }
         else{
-            singleView.isHidden = false
+            singleScrollView.isHidden = false
             instrumentCollectionView.isHidden = true
         }
         
     }
     
 }
+
+// 2. collection view code
 
 extension ViewController : UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
